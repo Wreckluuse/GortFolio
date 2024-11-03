e3f2988a5e689087900d40fe98b8b0701149a2f4
Andrew Weightman <andrewjw797@gmail.com>
2024-11-03 15:44:00 -0800

    time for a redo

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
 		})
 	})
-
-	r.Static("/static", "./static")
+	r.Static("/static", "./static/")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
